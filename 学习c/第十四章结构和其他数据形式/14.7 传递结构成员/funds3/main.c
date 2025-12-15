5a27429d6b890018e98f097e077069e2369d2caa
spojchil <2069510482@qq.com>
2025-12-16 07:01:50 +0800

    feat: 完成14.6-14.7.4指向结构的指针和向函数传递结构的信息

---
+++ 学习c/第十四章结构和其他数据形式/14.7 传递结构成员/funds3/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define FUNDLEN 50
+
+struct funds {
+    char bank[FUNDLEN];
+    double bankfund;
+    char save[FUNDLEN];
+    double savefund;
+};
+
+double sum(const struct funds);
+
+int main(void){
+    struct funds stan = {
+        "Farlic-Melon Bank",
+        4032.27,
+        "Lucky's Savings and Loan",
+        8543.94
+    };
+
+    printf("Stan has a total of $%.2f.\n", sum(stan));
+
+    return 0;
+}
+
+double sum(const struct funds moolah){
+    return(moolah.bankfund + moolah.savefund);
+}
